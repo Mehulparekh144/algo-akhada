948b64089b61eb9d1320632a76daa3fd28d33baf
Mehulparekh144 <mehulparekh144@gmail.com>
2025-02-05 17:59:33 -0500

    Add utility function for merging CSS classes to improve code readability and maintainability

---
+++ src/env.ts
@@ -0,0 +1,24 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().min(1),
+    DATABASE_URL_UNPOOLED: z.string().min(1),
+    POSTGRES_URL: z.string().min(1),
+    POSTGRES_URL_NON_POOLING: z.string().min(1),
+    POSTGRES_USER: z.string().min(1),
+    POSTGRES_HOST: z.string().min(1),
+    POSTGRES_PASSWORD: z.string().min(1),
+    POSTGRES_DATABASE: z.string().min(1),
+    POSTGRES_URL_NO_SSL: z.string().min(1),
+    POSTGRES_PRISMA_URL: z.string().min(1),
+    GITHUB_SECRET: z.string().min(1),
+  },
+  client: {
+    NEXT_PUBLIC_GITHUB_ID: z.string().min(1)
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_GITHUB_ID: process.env.NEXT_PUBLIC_GITHUB_ID
+  }
+})
