0a2b47283c05201204291b089e0dfbab4e840cb4
Mehulparekh144 <mehulparekh144@gmail.com>
2025-03-01 19:40:08 -0500

    refactor: clean up unused imports in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { LcProblem } from "./validations";
-import { headers } from "next/headers";
-import { auth } from "./auth";
-import { Session, User } from "better-auth";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
