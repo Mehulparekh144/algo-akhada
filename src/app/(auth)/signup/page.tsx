b169ba26dcd05f7ba0f2a93172c9c24f3ae9aa21
Mehulparekh144 <mehulparekh144@gmail.com>
2025-03-01 22:14:13 -0500

    feat: add TipSection component and integrate it into signin and signup pages; update global styles and theme configuration

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,17 +2,23 @@ import { redirect } from "next/navigation";
 import React from "react";
 import Signup from "./Signup";
 import { getUser } from "@/app/actions";
+import TipSection from "../TipSection";
 
 export default async function SigninPage() {
-	const session = await getUser();
+  const session = await getUser();
 
-	if (session) {
-		redirect("/dashboard");
-	}
+  if (session) {
+    redirect("/dashboard");
+  }
 
-	return (
-		<div className="border min-h-screen flex justify-center items-center">
-			<Signup />
-		</div>
-	);
+  return (
+    <div className="border min-h-screen bg-secondary grid grid-cols-1 lg:grid-cols-2">
+      <div className="md:mx-16 lg:mx-20 mx-4 h-full flex items-center justify-center">
+        <Signup />
+      </div>
+      <div className="hidden relative lg:flex items-center justify-center dark:bg-black bg-white">
+        <TipSection />
+      </div>
+    </div>
+  );
 }
