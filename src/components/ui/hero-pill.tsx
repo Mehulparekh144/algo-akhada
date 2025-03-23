8f5282c78b4a54de7199f407e117fd3ce06945df
Mehulparekh144 <mehulparekh144@gmail.com>
2025-03-22 23:24:02 -0400

    fix: update SVG title to aria-label for improved accessibility in HeroPill component

--- src/components/ui/hero-pill.tsx
+++ src/components/ui/hero-pill.tsx
@@ -47,7 +47,8 @@ export function HeroPill({
 				viewBox="0 0 12 12"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
-				title="Arrow right"
+				aria-label="Arrow right"
+				
 			>
 				<path
 					d="M8.78141 5.33312L5.20541 1.75712L6.14808 0.814453L11.3334 5.99979L6.14808 11.1851L5.20541 10.2425L8.78141 6.66645H0.666748V5.33312H8.78141Z"
